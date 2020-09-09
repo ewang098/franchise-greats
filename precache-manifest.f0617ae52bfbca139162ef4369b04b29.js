2da6f7efb09b7efcc6836751c97d75cdca9ea579
ewang098 <61338151+ewang098@users.noreply.github.com>
2020-09-08 23:38:36 -0700

    Updates

---
+++ precache-manifest.f0617ae52bfbca139162ef4369b04b29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5efcac65c852c1611da407bf0a7c27a",
+    "url": "/franchise-greats/index.html"
+  },
+  {
+    "revision": "92e573cc3ca348f11fe8",
+    "url": "/franchise-greats/static/css/main.9253476f.chunk.css"
+  },
+  {
+    "revision": "f5fb27b025fb68ae0c79",
+    "url": "/franchise-greats/static/js/2.955bbb04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/franchise-greats/static/js/2.955bbb04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e573cc3ca348f11fe8",
+    "url": "/franchise-greats/static/js/main.31df6c9e.chunk.js"
+  },
+  {
+    "revision": "1e51659fdefff94b1a52",
+    "url": "/franchise-greats/static/js/runtime-main.2a845ff0.js"
+  },
+  {
+    "revision": "3510d7ae0fae04d1fdfc312a47af8590",
+    "url": "/franchise-greats/static/media/NBAEastern.3510d7ae.svg"
+  },
+  {
+    "revision": "4cd0c1fd910540766ad3447be2f7d7c8",
+    "url": "/franchise-greats/static/media/NBAWestern.4cd0c1fd.svg"
+  }
+]);
